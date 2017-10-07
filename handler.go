1f03847d1b0048447b8db98c2fd65b3d2d02a23c
Long Do <me@dotronglong.com>
2017-10-07 18:00:12 +0700

    Update follow lapi

--- handler.go
+++ handler.go
@@ -1,6 +1,7 @@
 package cors
 
 import (
+	"github.com/goline/errors"
 	. "github.com/goline/lapi"
 )
 
@@ -8,7 +9,7 @@ type corsHandler struct {
 	Policy Policy
 }
 
-func (h *corsHandler) Handle(c Connection) (interface{}, error) {
+func (h *corsHandler) Handle(c Connection) (interface{}, errors.Error) {
 	response := c.Response()
 	h.Policy.Apply(response.Header())
 	return nil, response.Send()
--- hook.go
+++ hook.go
@@ -1,6 +1,7 @@
 package cors
 
 import (
+	"github.com/goline/errors"
 	. "github.com/goline/lapi"
 )
 
@@ -8,12 +9,12 @@ type corsHook struct {
 	Policy Policy
 }
 
-func (h *corsHook) SetUp(c Connection) error {
+func (h *corsHook) SetUp(c Connection) errors.Error {
 	h.Policy.Apply(c.Response().Header())
 
 	return nil
 }
 
-func (h *corsHook) TearDown(_ Connection, _ interface{}, _ error) error {
+func (h *corsHook) TearDown(_ Connection, _ interface{}, _ errors.Error) errors.Error {
 	return nil
 }
